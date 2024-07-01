f68dee2d8b33bc8708d60317b526bcefd33ea841
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-07-01 13:37:59 +0400

    a

--- single-service.php
+++ single-service.php
@@ -303,6 +303,6 @@ if ($feture_sec == 'yes'): ?>
     </div>
 </section> -->
 
-<?php get_template_part('templates/partners'); ?>
+
 <?php get_template_part('templates/contact'); ?>
 <?Php get_footer(); ?>
